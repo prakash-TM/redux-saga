a4128ca6539b4f9da2c05b36f4497b1a063b3465
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2021-12-21 22:05:57 +0530

    modified the src

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reducer from "./store/reducer";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { watchAgeUp } from "./sagas/saga";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(watchAgeUp);
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </Provider>,
+  document.getElementById("root")
+);
